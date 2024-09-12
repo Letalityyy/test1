7207b2b486c1f9c5ef8d691b2d97387d4bad4aa9
PManuel15 <manuel.pons@laposte.net>
2024-09-12 08:42:34 +0200

    fix nom fariable

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -2,56 +2,56 @@ package fr.iut.editeur.document;
 
 public class Document {
 
-    private String texte;
+    private String contentDocument;
 
     public Document() {
-        this.texte = "";
+        this.contentDocument = "";
     }
 	
     public String getTexte() {
-        return texte;
+        return contentDocument;
     }
 
     public void setTexte(String texte) {
-        this.texte = texte;
+        this.contentDocument = texte;
     }
 
     public void ajouter(String texte) {
-        this.texte += texte;
+        this.contentDocument += texte;
     }
 
     public void effecer(String texte){
-        this.texte.replace(this.texte,texte);
+        this.contentDocument.replace(this.contentDocument,texte);
     }
 
     public void clear(){
-        this.texte = "";
+        this.contentDocument = "";
 
     }
 
     public void inserer(int debut, String remplacement){
-        String partieGauche = texte.substring(0, debut);
-        String partieDroite = texte.substring(debut);
-        texte= partieGauche + remplacement + partieDroite;
+        String partieGauche = contentDocument.substring(0, debut);
+        String partieDroite = contentDocument.substring(debut);
+        contentDocument= partieGauche + remplacement + partieDroite;
 
     }
 
     public void remplacer(int debut, int fin, String remplacement) {
-        String partieGauche = texte.substring(0, debut);
-        String partieDroite = texte.substring(fin + 1);
-        texte = partieGauche + remplacement + partieDroite;
+        String partieGauche = contentDocument.substring(0, debut);
+        String partieDroite = contentDocument.substring(fin + 1);
+        contentDocument = partieGauche + remplacement + partieDroite;
     }
 
     public void majuscules(int debut, int fin) {
         //TO-DO!
     }
     public void minuscules(int debut, int fin){
-        String partie = texte.substring(debut, fin+1);
+        String partie = contentDocument.substring(debut, fin+1);
         partie =partie.toLowerCase();
         remplacer(debut, fin, partie);
     }
     @Override
     public String toString() {
-        return this.texte;
+        return this.contentDocument;
     }
 }
